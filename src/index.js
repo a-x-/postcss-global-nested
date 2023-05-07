74c94266c9fce522ef0ee83da7fd893bb8f89579
Robert Hall <arxpoetica@users.noreply.github.com>
2023-05-06 21:00:59 -0600

    Update to PostCSS 8.0 API
    See: https://evilmartians.com/chronicles/postcss-8-plugin-migration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-var postcss = require('postcss');
-
-module.exports = postcss.plugin("postcss-global-nested", () => {
-  return root => {
-    root.walkRules(rule => {
-      rule.selector = rule.selector
-        .split(",")
-        .map(selector => selector.trim().replace(/:global\s+([\w\W]+)/, ":global($1)"))
-        .join(", ")
-    });
+const globalNested = () => {
+  return {
+    postcssPlugin: "postcss-global-nested",
+    Rule(rule) {
+      if (rule.selector.includes(":global")) {
+        rule.selector = rule.selector
+          .split(",")
+          .map(selector => selector.trim().replace(/:global\s+([\w\W]+)/, ":global($1)"))
+          .join(", ");
+      }
+    },
   };
-});
+};
+
+globalNested.postcss = true;
+
+module.exports = globalNested;
